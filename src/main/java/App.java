5f66366bf01fb256cd189ff893c2967a52b90835
abhinav-dawar <abhinav.dawar@tothenew.com>
2021-02-25 16:11:18 +0530

    initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println(returnSomething());
+    }
+    public static String returnSomething() {
+        return "Hello World";
+    }
+}
